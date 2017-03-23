d221a685129b519bf4860593cc2dfd00bb81758a
ggjimmy <jirkazboril4@seznam.cz>
2017-03-23 18:12:46 +0100

    Added basic camera

--- app/src/main/java/com/example/ggjimmy/mobilebroadcast/MainActivity.java
+++ app/src/main/java/com/example/ggjimmy/mobilebroadcast/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.ggjimmy.mobilebroadcast;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Camera camera = null;
+    private CameraView cameraView = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        camera = Camera.open();
+        cameraView = new CameraView(this, camera);
+        FrameLayout layout = (FrameLayout) findViewById(R.id.camera_view);
+        layout.addView(cameraView);
     }
 }
