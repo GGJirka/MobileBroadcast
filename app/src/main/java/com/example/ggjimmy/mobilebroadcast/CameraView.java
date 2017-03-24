e6344a64301f527c4b3ae85a5b262fc2d7dead37
ggjimmy <jirkazboril4@seznam.cz>
2017-03-24 21:06:18 +0100

    added camera rotation

---
+++ app/src/main/java/com/example/ggjimmy/mobilebroadcast/CameraView.java
@@ -0,0 +1,56 @@
+package com.example.ggjimmy.mobilebroadcast;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * Created by ggjimmy on 3/23/17.
+ */
+
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
+    private SurfaceHolder holder;
+    private Camera camera;
+
+    public CameraView(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if(holder != null)
+            return;
+
+        camera.stopPreview();
+
+        try{
+            camera.setPreviewDisplay(holder);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+    }
+}
--- app/src/main/java/com/example/ggjimmy/mobilebroadcast/MainActivity.java
+++ app/src/main/java/com/example/ggjimmy/mobilebroadcast/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.ggjimmy.mobilebroadcast;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Camera camera = null;
+    private CameraView cameraView = null;
+    private boolean state = false;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        camera = Camera.open(0);
+        cameraView = new CameraView(this, camera);
+        FrameLayout layout = (FrameLayout) findViewById(R.id.camera_view);
+        layout.addView(cameraView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.camera_rotation:
+                FrameLayout layout = (FrameLayout) findViewById(R.id.camera_view);
+                layout.removeAllViews();
+                camera = !state ? Camera.open(0) : Camera.open(1);
+                cameraView = new CameraView(this, camera);
+                layout.addView(cameraView);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
